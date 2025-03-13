21b95a77ad0802e80957459ba61036febf0a15a4
Typedin <antoine@typedin.dev>
2025-03-13 22:39:37 +0100

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()], // Enable path alias resolution
-  resolve: {
-    alias: {
-      "@": "/src", // This allows you to use @ as an alias for src/
-    },
-    // Ensure aliasing also works with test files
-    test: {
-      globals: true,
-      environment: "node",
-    },
-  },
+	plugins: [tsconfigPaths()], // Enable path alias resolution
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src'), // Alias '@' to 'src/' directory
+		},
+	},
 });
---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		globals: true, // Enable global test functions like describe, test, expect
+		environment: 'node', // Set the environment to Node.js
+	},
+});
+
