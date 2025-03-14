7b3ea4cedccd10e9fa691d4fbad834438b7b811f
Typedin <antoine@typedin.dev>
2025-03-14 11:50:23 +0100

    wip: fixing types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,58 +1,63 @@
 import { AlterationEnum, DiatonicNoteEnum } from "../constants";
+
 export type IScale = any;
+
+export interface IReference extends Note {
+  frequency: number;
+}
+
 export interface Interval {
-	direction?: "up" | "down";
-	compound: boolean;
-	halfsteps: number;
-	quality?: string;
-	export type: string;
+  direction?: "up" | "down";
+  compound: boolean;
+  halfsteps: number;
+  quality?: string;
+  type: string;
 }
 
 export interface Note {
-	name: DiatonicNoteEnum;
-	alteration: AlterationEnum;
-	octave: number;
+  name: DiatonicNoteEnum;
+  alteration: AlterationEnum;
+  octave: number;
 }
 
 export interface IScaleDegree extends Note {
-	degree: number; // the actual musical degree
-	function: string;
-	order: number; // used to sort them
+  degree: number; // the actual musical degree
+  function: string;
+  order: number; // used to sort them
 }
+
 export interface IMusicalString extends Note {
-	frequency: number;
+  frequency: number;
 }
 
 export interface ReferenceUsedInStringStore extends Note {
-	tuning: number;
+  tuning: number;
 }
 
 export interface Alteration {
-	name: string;
-	visual:
-	| AlterationEnum.flat
-	| AlterationEnum.sharp
-	| AlterationEnum.natural
-	| AlterationEnum.doubleFlat
-	| AlterationEnum.doubleSharp;
+  name: string;
+  visual:
+  | AlterationEnum.flat
+  | AlterationEnum.sharp
+  | AlterationEnum.natural
+  | AlterationEnum.doubleFlat
+  | AlterationEnum.doubleSharp;
 }
 
 export type IntervalBuilder = (
-	note: Note,
-	direction: "up" | "down" = "up"
+  note: Note,
+  direction: "up" | "down" = "up",
 ) => Note;
 
 export interface StringInstumentPreset {
-	strings: Array<IMusicalString>;
-	stringLimits: Array<StringLimits>;
+  strings: Array<IMusicalString>;
+  stringLimits: Array<StringLimits>;
 }
 
 export interface StringLimits {
-	string: IMusicalString;
-	limits: {
-		upper: Note;
-		lower: Note;
-	};
+  string: IMusicalString;
+  limits: {
+    upper: Note;
+    lower: Note;
+  };
 }
-
-export { };
