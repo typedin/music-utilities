8a94eca887a3c117a708c6a8acc749213572bfe0
Typedin <antoine@typedin.dev>
2025-03-14 11:01:17 +0100

    prettier && eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,30 @@
-import globals  from "globals";
+import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import vitest from "eslint-plugin-vitest";
+import prettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {
-	...globals.browser, 
-	...globals.node, 
-	...vitest.environments.env.globals
-    } }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...vitest.environments.env.globals,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: { prettier }, // Add Prettier plugin
+    rules: {
+      ...prettier.configs.recommended.rules, // Use Prettier's recommended rules
+    },
+  },
+  eslintConfigPrettier, // Disable conflicting ESLint rules
 ];
+
