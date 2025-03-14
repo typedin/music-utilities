38183f64a27c1b93a4df36403b40acf109cb28d6
Typedin <antoine@typedin.dev>
2025-03-14 10:54:09 +0100

    wip: fixing types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,16 @@
-import globals from "globals";
+import globals  from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import vitest from "eslint-plugin-vitest";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {languageOptions: { globals: {
+	...globals.browser, 
+	...globals.node, 
+	...vitest.environments.env.globals
+    } }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
