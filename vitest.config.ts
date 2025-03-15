2283b1bf57986fbd211e9dc6aa1eb946db4e22ad
Typedin <antoine@typedin.dev>
2025-03-15 12:08:08 +0100

    test with vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 export default defineConfig({
   test: {
     globals: true, // Enable global test functions like describe, test, expect
     environment: "node", // Set the environment to Node.js
     includeSource: ["src/**/*.ts"],
+  },
+  plugins: [tsconfigPaths()], // Enable path alias resolution
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      // Add other aliases here
+    },
   },
 });
