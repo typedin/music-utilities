7c839dee35b40e946fa7519874b766dd88c9b1c2
Typedin <antoine@typedin.dev>
2025-03-16 16:53:50 +0100

    rexports scale utilities from index.ts file

--- src/scale-builders/index.ts
+++ src/scale-builders/index.ts
@@ -1,1 +1,7 @@
 // TODO
+import { ScaleBuilder } from "./ScaleBuilder.js";
+import { beginOn } from "./beginOn.js";
+import { endOn } from "./endOn.js";
+import { spanOver } from "./spanOver.js";
+
+export { ScaleBuilder, beginOn, endOn, spanOver }
